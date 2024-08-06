42a0fe2c9a103d42639ccda3898198265e6365dc
abhik1424 <abhik1424@gmail.com>
2024-08-06 08:47:02 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>Hi i am H1 Tag</h1>
+ *      <h2>HI I am H2 Tag<h2>
+ *  </div>
+ *  <div id="child">
+ *      <h1>Hi i am H1 Tag</h1>
+ *      <h2>HI I am H2 Tag<h2>
+ *  </div>
+ * </div>
+**/
+
+
+
+const heading = React.createElement('div', {id: 'parent'}, 
+    [React.createElement('div', {id: 'child'},
+        [React.createElement('h1', {id: 'heading'}, 'Hi I am H1 Tag'),
+        React.createElement('h2', {id: 'headingh2'}, 'Hi I am H2 Tag')]
+    ),
+    React.createElement('div', {id: 'child'},
+        [React.createElement('h1', {id: 'heading1'}, 'Hi I am H1 Tag'),
+        React.createElement('h2', {id: 'headingh21'}, 'Hi I am H2 Tag')]
+    )]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
