d85024faec747ba46ed0d29b31ce31274a476fbc
abhik1424 <abhik1424@gmail.com>
2024-08-16 20:40:25 +0530

    igniting our App

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1>Hi i am H1 Tag</h1>
- *      <h2>HI I am H2 Tag<h2>git 
- *  </div>
- *  <div id="child">
- *      <h1>Hi i am H1 Tag</h1>
- *      <h2>HI I am H2 Tag<h2>
- *  </div>
- * </div>
-**/
-
-
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 
 const heading = React.createElement('div', {id: 'parent'}, 
-    [React.createElement('div', {id: 'child'},
-        [React.createElement('h1', {id: 'heading'}, 'Hi I am H1 Tag'),
-        React.createElement('h2', {id: 'headingh2'}, 'Hi I am H2 Tag')]
-    ),
-    React.createElement('div', {id: 'child'},
-        [React.createElement('h1', {id: 'heading1'}, 'Hi I am H1 Tag'),
-        React.createElement('h2', {id: 'headingh21'}, 'Hi I am H2 Tag')]
-    )]
+    [
+        React.createElement('div', {id: 'child'},
+            [
+                React.createElement('h1', {id: 'heading'}, 'This is Namaste React'),
+                React.createElement('h2', {id: 'headingh2'}, 'Hi I am H2 Tag')
+            ]
+        ),
+        React.createElement('div', {id: 'child'},
+            [
+                React.createElement('h1', {id: 'heading1'}, 'Hi I am H1 Tag'),
+                React.createElement('h2', {id: 'headingh21'}, 'Hi I am H2 Tag')
+            ]
+        )
+    ]
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Faster Build - Caching
+- Image Optimaization
+- Minification File
+- Bundling All Thing
+- Compress 
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove Unused Code
+- Diffrent Build for Dev & Production
